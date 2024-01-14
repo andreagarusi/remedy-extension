8e3f7ae0cf492cd9093884c30fc08ca947b180e9
Andrea Garusi <74213822+andreagarusi@users.noreply.github.com>
2024-01-14 17:41:24 +0100

    .

--- scripts/windowMiniaturized.js
+++ scripts/windowMiniaturized.js
@@ -1,48 +1,71 @@
-const divNoteContainerIds = ["WIN_1_1000000151", "WIN_3_1000000151", "WIN_4_1000000151", "WIN_5_1000000151"];
-const paramsBuilderNetEyeUrl = ["arid_WIN_3_1000000151", "https://monitor.irideos.it/neteye/search?q=", "#!/neteye/monitoring/host/show?host=", "https://monitor.irideos.it/neteye/dashboard"];
-const regexNetEye = /[A-Za-z0-9]+-[A-Za-z0-9]+@([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)_[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/;;
-
-function addButtonToDiv(divElement, baseBtnId, btnText, paramsBuilderUrl, btnRegex) {
-    const buttonId = `${baseBtnId}_${divElement}`;
-
-    if (!divElement.querySelector(`#${buttonId}`)) {
-        const netEyeButton = document.createElement("button");
-        netEyeButton.textContent = btnText;
-        netEyeButton.id = buttonId;
-        netEyeButton.onclick = function() {
-            const buttonUrl = buildButtonUrl(paramsBuilderUrl, btnRegex);
-            openWindowMiniaturized(buttonUrl);
-        };
-
-        divElement.appendChild(netEyeButton);
-    }
-}
-
 function addBtnNetEye() {
-    divNoteContainerIds.forEach(identifier => {
-        const divElement = document.getElementById(identifier);
-        addButtonToDiv(divElement, "btnNetEye", "", paramsBuilderNetEyeUrl, regexNetEye);
-    });
+    const mainDivNote = document.getElementById("WIN_3_1000000151");
+    const searchDivNote = document.getElementById("WIN_4_1000000151");
+    const searchDivNoteTwo = document.getElementById("WIN_5_1000000151");
+
+    if (mainDivNote) {
+        if (!mainDivNote.querySelector("#btnNetEyeMain")) {
+            var btnNetEyeMain = document.createElement("button");
+            btnNetEyeMain.textContent = "";
+            btnNetEyeMain.id = "btnNetEyeMain";
+            btnNetEyeMain.onclick = function() {
+                var NetEyeUrl = buildNetEyeUrl();
+                openWindowMiniaturized(NetEyeUrl);
+            };
+
+            mainDivNote.appendChild(btnNetEyeMain);
+        }
+    }
+
+    if (searchDivNote) {
+        if (!searchDivNote.querySelector("#btnNetEyeSearch")) {
+            var btnNetEyeSearch = document.createElement("button");
+            btnNetEyeSearch.textContent = "";
+            btnNetEyeSearch.id = "btnNetEyeSearch";
+            btnNetEyeSearch.onclick = function() {
+                var NetEyeUrl = buildNetEyeUrl();
+                openWindowMiniaturized(NetEyeUrl);
+            };
+
+            searchDivNote.appendChild(btnNetEyeSearch);
+        }
+    }
+
+    if (searchDivNoteTwo) {
+        if (!searchDivNoteTwo.querySelector("#btnNetEyeSearchTwo")) {
+            var btnNetEyeSearch = document.createElement("button");
+            btnNetEyeSearch.textContent = "";
+            btnNetEyeSearch.id = "btnNetEyeSearch";
+            btnNetEyeSearch.onclick = function() {
+                var NetEyeUrl = buildNetEyeUrl();
+                openWindowMiniaturized(NetEyeUrl);
+            };
+
+            searchDivNote.appendChild(btnNetEyeSearch);
+        }
+    }
 }
 
 // Esegui la funzione addBtnNetEye ogni 1,5 secondo
 setInterval(addBtnNetEye, 1500);
 
-
 // Funzione per estrarre hostname dal campo Note, restituisce url NetEye
-function buildButtonUrl(paramsBuilderUrl, regex) {
-    var noteContent = document.getElementById(paramsBuilderUrl[0]).value;
+function buildNetEyeUrl() {
+    var noteContent = document.getElementById("arid_WIN_3_1000000151").value;
+    var regex = /[A-Za-z0-9]+-[A-Za-z0-9]+@([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)_[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/;
     var matchHostname = noteContent.match(regex);
 
     if (matchHostname) {
         // console.log(matchHostname);
-        return baseBtnUrl[1] + matchHostname[0] + baseBtnUrl[2] + matchHostname[0];
+        return "https://monitor.irideos.it/neteye/search?q=" + matchHostname[0] + "#!/neteye/monitoring/host/show?host=" + matchHostname[0];
     }
 
-    return baseBtnUrl[3];
+    // Restituisce url della dashboard NetEye se nessun match è stato trovato
+    return "https://monitor.irideos.it/neteye/dashboard";
 }
 
 function openWindowMiniaturized(pageUrl) {
+    // Specifica le dimensioni della finestra in miniatura
     var windowWidth = 1200;
     var windowHeight = 550;
 
@@ -50,5 +73,6 @@ function openWindowMiniaturized(pageUrl) {
     var xPos = (window.innerWidth - windowWidth) / 2;
     var yPos = (window.innerHeight - windowHeight) / 2;
 
+    // Apertura di una nuova finestra con l'URL e le dimensioni specificate
     window.open(pageUrl, "Window Miniaturized", "width=" + windowWidth + ", height=" + windowHeight + ", top=" + yPos + ", left=" + xPos);
 }
