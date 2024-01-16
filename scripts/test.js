274896ea4f83f9dd0b9aa5f7b1b5d8460a14795a
Andrea Garusi <74213822+andreagarusi@users.noreply.github.com>
2024-01-16 21:05:55 +0100

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -4,14 +4,14 @@ function addBtnNetEye() {
 
     divElements.forEach(function(divElement) {
         const match = divElement.id.match(regex);
-        if (match && !divElement.querySelector("#btnNetEye_" + match[1])) {
+        if (match && !divElement.querySelector("#btnNetEye_" + match[0])) {
             var btnNetEye = document.createElement("button");
             btnNetEye.textContent = "";
-            btnNetEye.id = "btnNetEye_" + match[1];
+            btnNetEye.id = "btnNetEye_" + match[0];
             console.log(btnNetEye.id);
             btnNetEye.classList.add("btnNetEye");
             btnNetEye.onclick = function() {
-                var NetEyeUrl = buildNetEyeUrl(match[1]);
+                var NetEyeUrl = buildNetEyeUrl(match[0]);
                 openWindowMiniaturized(NetEyeUrl);
             };
 
@@ -24,9 +24,10 @@ setInterval(addBtnNetEye, 1500);
 
 
 function buildNetEyeUrl(containerId) {
-    var textareaId = "arid_WIN_" + containerId.substring(0, containerId.lastIndexOf("_")) + "_1000000151";
-
-    // Ottieni il valore della textarea
+    console.log(containerId)
+    var textareaId = "arid_" + containerId.substring(0, containerId.lastIndexOf("_")) + "_1000000151";
+    console.log(textareaId)
+        // Ottieni il valore della textarea
     var noteContent = document.getElementById(textareaId).value;
 
     var regex = /[A-Za-z0-9]+-[A-Za-z0-9]+@([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)_[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/;
