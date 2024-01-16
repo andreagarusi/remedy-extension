e7ac0b5c13697e882eee688e86579a2fb6ded20f
Andrea Garusi <74213822+andreagarusi@users.noreply.github.com>
2024-01-16 21:06:32 +0100

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -8,7 +8,6 @@ function addBtnNetEye() {
             var btnNetEye = document.createElement("button");
             btnNetEye.textContent = "";
             btnNetEye.id = "btnNetEye_" + match[0];
-            console.log(btnNetEye.id);
             btnNetEye.classList.add("btnNetEye");
             btnNetEye.onclick = function() {
                 var NetEyeUrl = buildNetEyeUrl(match[0]);
@@ -24,12 +23,8 @@ setInterval(addBtnNetEye, 1500);
 
 
 function buildNetEyeUrl(containerId) {
-    console.log(containerId)
     var textareaId = "arid_" + containerId.substring(0, containerId.lastIndexOf("_")) + "_1000000151";
-    console.log(textareaId)
-        // Ottieni il valore della textarea
     var noteContent = document.getElementById(textareaId).value;
-
     var regex = /[A-Za-z0-9]+-[A-Za-z0-9]+@([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.([A-Za-z0-9]+(-[A-Za-z0-9]+)+)\.[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)_[A-Za-z0-9]+|([A-Za-z0-9]+(-[A-Za-z0-9]+)+)/;
     var matchHostname = noteContent.match(regex);
 
