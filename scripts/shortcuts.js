6269965f8fb54814435e0c5bbfeb9df749b2e9bb
Andrea Garusi <74213822+andreagarusi@users.noreply.github.com>
2024-02-11 21:39:26 +0100

    Update shortcuts.js

--- scripts/shortcuts.js
+++ scripts/shortcuts.js
@@ -28,4 +28,37 @@ document.addEventListener('keydown', function(event) {
         document.getElementById("WIN_0_304248650").click();
         event.preventDefault();
     }
+
+    // Nuovo incidente
+    if (event.ctrlKey && event.shiftKey && (event.key === 'n' || event.key === 'N')) {
+        var targetElement = findElementWithText("Nuovo incidente");
+
+        if (targetElement) {
+            targetElement.click();
+        }
+
+        event.preventDefault();
+    }
+
+    // Ricerca incidente
+    if (event.ctrlKey && event.shiftKey && (event.key === 'f' || event.key === 'F')) {
+        var targetElement = findElementWithText("Ricerca incidente");
+
+        if (targetElement) {
+            targetElement.click();
+        }
+
+        event.preventDefault();
+    }
+
 });
+
+function findElementWithText(text) {
+    var elements = document.querySelectorAll('a.btn span.navLabel');
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].textContent.trim() === text) {
+            return elements[i].closest('a.btn');
+        }
+    }
+    return null;
+}
