649c568b3cc4a5a442761cc475b5ae2babcdca65
Andrea Garusi <74213822+andreagarusi@users.noreply.github.com>
2024-02-12 00:12:50 +0100

    Update shortcuts.js

--- scripts/shortcuts.js
+++ scripts/shortcuts.js
@@ -1,32 +1,46 @@
 document.addEventListener('keydown', function(event) {
     // Salva
     if (event.ctrlKey && (event.key === 's' || event.key === 'S')) {
-        document.querySelector('[id$="301614800"]').click();
+        var elem = document.querySelector('[id$="301614800"]');
+        if (elem) {
+            elem.click();
+        }
         event.preventDefault();
     }
 
     // Aggiorna
     if (event.ctrlKey && (event.key === 'r' || event.key === 'R')) {
-        document.querySelector('[id$="301444200"] > div.TableHdr > table > tbody > tr > td.TableHdrR > a.Ref.btn.btn3d.TableBtn').click();
+        var elem = document.querySelector('[id$="301444200"] > div.TableHdr > table > tbody > tr > td.TableHdrR > a.Ref.btn.btn3d.TableBtn');
+        if (elem) {
+            elem.click();
+        }
         event.preventDefault();
     }
 
     // Aggiungi
     if (event.ctrlKey && (event.key === 'Enter' || event.key === 'NumpadEnter')) {
-        document.querySelector('[id$="304247110"]').click();
+        var elem = document.querySelector('[id$="304247110"]');
+        if (elem) {
+            elem.click();
+        }
         event.preventDefault();
     }
 
     // Back
     if (event.altKey && event.key === 'ArrowLeft') {
-        document.querySelector('[id$="304248620"]').click();
+        var elem = document.querySelector('[id$="304248620"]')
+        if (elem) {
+            elem.click();
+        }
         event.preventDefault();
     }
 
     // Next
     if (event.altKey && event.key === 'ArrowRight') {
-        document.querySelector('[id$="304248650"]').click();
+        var elem = document.querySelector('[id$="304248650"]');
+        if (elem) {
+            elem.click();
+        }
         event.preventDefault();
     }
-
 });
